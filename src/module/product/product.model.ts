91fd7ff1c50e9e3a907f4d7bb517a1b67b413239
asad406 <asadul76@gmail.com>
2024-11-26 20:40:19 +0600

    book interface and bock model added

---
+++ src/module/product/product.model.ts
@@ -0,0 +1,36 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "./product.interface";
+
+const bookSchema = new Schema<IBook>({
+    title: {
+        type: String,
+        required: [true, "Title required"]
+    },
+    author: {
+        type: String,
+        required: [true, 'Author is required']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required']
+    },
+    category: {
+        type: String,
+        required: [true, 'Category is required'],
+        enum: ['Fiction', 'Science', 'SelfDevelopment', 'Poetry', 'Religious']
+    },
+    description: {
+        type: String,
+        required: [true, 'Category is required']
+    },
+    inStock: {
+        type: Boolean,
+        required: [true, 'Stock is required']
+    }
+},
+    {
+        timestamps: true
+    }
+)
+const Book = model<IBook>('book', bookSchema)
+export default Book
